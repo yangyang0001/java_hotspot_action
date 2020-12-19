b65973c4752eecc864bc27321f8342afdd2b673e
yangjianwei <2017585616@qq.com>
2020-12-20 06:24:46 +0800

    Centos

---
+++ src/main/java/com/deepblue/chapter_02_compile/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export LANG=C
+
+#导入JDK路径
+export ALT_BOOTDIR="/home/jdk1.6.0_35"
+export ALT_JDK_IMPORT_PATH="/home/jdk1.6.0_35"
+
+#导入ANT路径
+export ANT_HOME="/home/apache-ant-1.8.4"
+
+#导入PATH
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+
+#其他配置
+export HOTSPOT_BUILD_JOBS=5
+
+#编译后的输出目录
+export ALT_OUTPUTDIR=/home/openjdk/hotspot/build_jvm
+
+#编译模式jvm采用64位
+export ARCH_DATA_MODEL=64
+
+cd make
+make jvmg jvmg1 2>&1 | tee /home/openjdk/hotspot/build_jvm/hotspot_debug.log
