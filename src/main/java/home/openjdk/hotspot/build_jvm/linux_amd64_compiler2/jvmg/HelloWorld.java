5a69638952db980cb4131a9a3fa605e2102f0af6
yangjianwei <2017585616@qq.com>
2021-05-04 04:36:04 +0800

    hotspot in action compiler

---
+++ src/main/java/home/openjdk/hotspot/build_jvm/linux_amd64_compiler2/jvmg/HelloWorld.java
@@ -0,0 +1,8 @@
+package home.openjdk.hotspot.build_jvm.linux_amd64_compiler2.jvmg;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld HOTSPOT_IN_ACTION");
+    }
+}
