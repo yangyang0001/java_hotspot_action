ef77c1f460a55933db0fbdec06929b3542d55ae0
yangjianwei <2017585616@qq.com>
2021-05-20 00:32:26 +0800

    书写HelloWorld.c并使用GDB命令

---
+++ src/main/java/HelloWorld.c
@@ -0,0 +1,16 @@
+//
+// Created by 杨建伟 on 2021/5/19.
+//
+
+# include <stdio.h>
+
+int main() {
+    int i = 0;
+    printf("welcome hotspot in action!\n");
+    printf("i =  %d\n", i);
+    i++;
+    printf("i =  %d\n", i);
+    return 0;
+}
+
+
