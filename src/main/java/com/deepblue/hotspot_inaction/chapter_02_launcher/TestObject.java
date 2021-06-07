97929c3372b6303a9b800e6b6cae2c9e5d7f89f1
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-06-07 19:36:49 +0800

    HotSpot_Klass和instanceKlass比较

--- src/main/java/com/deepblue/hotspot_inaction/chapter_02_launcher/TestObject.java
+++ src/main/java/com/deepblue/hotspot_inaction/chapter_02_launcher/TestObject.java
@@ -3,6 +3,7 @@ package com.deepblue.hotspot_inaction.chapter_02_launcher;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import lombok.*;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -13,15 +14,32 @@ public class TestObject {
 
     public static void main(String[] args) throws Exception{
 
-        long JNI_VERSION_1_1 = 0x00010001;
-        long JNI_VERSION_1_2 = 0x00010002;
-        long JNI_VERSION_1_4 = 0x00010004;
-        long JNI_VERSION_1_6 = 0x00010006;
+//        long JNI_VERSION_1_1 = 0x00010001;
+//        long JNI_VERSION_1_2 = 0x00010002;
+//        long JNI_VERSION_1_4 = 0x00010004;
+//        long JNI_VERSION_1_6 = 0x00010006;
+//
+//        System.out.println("JNI_VERSION_1_1 = " + JNI_VERSION_1_1);
+//        System.out.println("JNI_VERSION_1_2 = " + JNI_VERSION_1_2);
+//        System.out.println("JNI_VERSION_1_4 = " + JNI_VERSION_1_4);
+//        System.out.println("JNI_VERSION_1_6 = " + JNI_VERSION_1_6);
 
-        System.out.println("JNI_VERSION_1_1 = " + JNI_VERSION_1_1);
-        System.out.println("JNI_VERSION_1_2 = " + JNI_VERSION_1_2);
-        System.out.println("JNI_VERSION_1_4 = " + JNI_VERSION_1_4);
-        System.out.println("JNI_VERSION_1_6 = " + JNI_VERSION_1_6);
+        List<String> merchantIds = Lists.newArrayList();
+        merchantIds.add("20150211ZH02030651");
+        String jsonIds = JSON.toJSONString(merchantIds);
+        System.out.println("jsonIds :" + jsonIds);
+
+        List<String> strings = JSON.parseArray(jsonIds, String.class);
+        System.out.println("strings :" + JSON.toJSONString(strings));
+
+        String merchantId = "20150211ZH02030651";
+        if(CollectionUtils.isNotEmpty(merchantIds) && !merchantIds.contains(merchantId)) {
+            // 来疯IOS退款成功订单 消息
+            System.out.println("来疯订单");
+        } else {
+            // TODO doNothing； 不区分来疯IOS和优酷IOS 则什么都不做! 目的就是接入来疯IOS退款成功消息
+            System.out.println("do Nothing");
+        }
 
     }
 
