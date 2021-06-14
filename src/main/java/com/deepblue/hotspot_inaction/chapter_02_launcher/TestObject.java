2ed17a49e7210f1b104f62a05f8f43b0e39a920a
yangjianwei <wb-yjw637332@alibaba-inc.com>
2021-06-14 19:43:48 +0800

    Java VisualVM 的使用

--- src/main/java/com/deepblue/hotspot_inaction/chapter_02_launcher/TestObject.java
+++ src/main/java/com/deepblue/hotspot_inaction/chapter_02_launcher/TestObject.java
@@ -8,7 +8,9 @@ import org.apache.commons.collections4.CollectionUtils;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class TestObject {
 
@@ -24,6 +26,36 @@ public class TestObject {
 //        System.out.println("JNI_VERSION_1_4 = " + JNI_VERSION_1_4);
 //        System.out.println("JNI_VERSION_1_6 = " + JNI_VERSION_1_6);
 
+//        List<Integer> allCodes0 = Lists.newArrayList(1, 2, 3);
+//        List<Integer> allCodes1 = Lists.newArrayList();
+//
+//        List<Integer> codes0 = Lists.newArrayList(1);
+//        List<Integer> codes1 = Lists.newArrayList(1, 2);
+//        List<Integer> codes2 = Lists.newArrayList(1, 2, 3);
+//        List<Integer> codes3 = Lists.newArrayList(2, 4);
+//        List<Integer> codes4 = Lists.newArrayList();
+//        List<Integer> codes5 = null;
+//
+//        System.out.println(allCodes0.containsAll(codes0));
+//        System.out.println(allCodes0.containsAll(codes1));
+//        System.out.println(allCodes0.containsAll(codes2));
+//        System.out.println(allCodes0.containsAll(codes3));
+//        System.out.println(allCodes0.containsAll(codes4));
+//        System.out.println(allCodes0.containsAll(codes5));
+
+//        System.out.println("1------------------------------------------------------");
+//
+//        System.out.println(allCodes1.containsAll(codes0));
+//        System.out.println(allCodes1.containsAll(codes1));
+//        System.out.println(allCodes1.containsAll(codes2));
+//        System.out.println(allCodes1.containsAll(codes3));
+//        System.out.println(allCodes1.containsAll(codes4));
+////        System.out.println(allCodes1.containsAll(codes5));
+//
+//        System.out.println("2------------------------------------------------------");
+
     }
+
+
 
 }
